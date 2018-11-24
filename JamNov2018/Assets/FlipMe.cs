5becec5101b4169ff17db302e2bc4b2ebd07ce5d
Flx <36176370+Khalios@users.noreply.github.com>
2018-11-24 14:23:56 -0500

    Rotation Spiky et Flip de tout le monde
    Instruction:
    - Replace Your Spiky with my Spiky Prefab
    - Put the Script Flip Me on every character (not the Parent, only the children)
    - Put the Physics Material Boiiing on Bouncy in the Collider2D

---
+++ JamNov2018/Assets/FlipMe.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipMe : MonoBehaviour {
+
+    private bool faceRight = true;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Mouse X");
+
+        if (move > 0 && !faceRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && faceRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        faceRight = !faceRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
