9fb207b6f3cf13ac39aa57bd8cee613e9381a9c5
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-11-23 22:14:58 -0500

    PushFinalJour1
    Felix, ta scène s'appelle ...PourFelix3

---
+++ JamNov2018/Assets/GroundedScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedScript : MonoBehaviour {
+
+	public bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		grounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Ground") {
+			print ("Je suis grounded");
+			grounded = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		if (other.gameObject.tag == "Ground") {
+			print ("Je suis pas grounded");
+			grounded = false;
+		}
+	}
+}
--- JamNov2018/Assets/Scripts/ClickManager.cs
+++ JamNov2018/Assets/Scripts/ClickManager.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class ClickManager : MonoBehaviour {
 
 	public int changeplayer;
+	public float jumpforce = 2;
 
 	public GameObject player1;
 	public GameObject player2;
 	public GameObject player3;
 	public GameObject player4;
 
+	Rigidbody2D rb1;
+	Rigidbody2D rb2;
+	Rigidbody2D rb3;
+	Rigidbody2D rb4;
+
+	GroundedScript groundedthescript1;
+	GroundedScript groundedthescript2;
+	GroundedScript groundedthescript3;
+	GroundedScript groundedthescript4;
+	public GameObject grounded1;
+	public GameObject grounded2;
+	public GameObject grounded3;
+	public GameObject grounded4;
+
 	void Start (){
 		changeplayer = 1;
+
+		rb1 = player1.GetComponent<Rigidbody2D> ();
+		rb2 = player2.GetComponent<Rigidbody2D> ();
+		rb3 = player3.GetComponent<Rigidbody2D> ();
+		rb4 = player4.GetComponent<Rigidbody2D> ();
+
+		groundedthescript1 = grounded1.GetComponent<GroundedScript> ();
+		groundedthescript2 = grounded2.GetComponent<GroundedScript> ();
+		groundedthescript3 = grounded3.GetComponent<GroundedScript> ();
+		groundedthescript4 = grounded4.GetComponent<GroundedScript> ();
 	}
 
 	void Update () {
@@ -29,7 +54,40 @@ public class ClickManager : MonoBehaviour {
 				Debug.Log(hit.collider.gameObject.name);
 				if (hit.collider.gameObject.tag == "Player") {
 					print ("Yassss");
-					changeplayer++;
+					if (changeplayer == 1 && groundedthescript1.grounded == true) { 
+						changeplayer++;
+					} else if (changeplayer == 2 && groundedthescript2.grounded == true) {
+						changeplayer++;
+					} else if (changeplayer == 3 && groundedthescript3.grounded == true) {
+						changeplayer++;
+					} else if (changeplayer == 4 && groundedthescript4.grounded == true) {
+						changeplayer++;
+					}
+				} 
+
+				if (hit.collider.gameObject.tag == "Background") {
+					print ("Je clique pas sur le player");
+					if (changeplayer == 1 && groundedthescript1.grounded == true) {
+						rb1.AddForce(new Vector2 (0, jumpforce));
+						rb2.AddForce(new Vector2 (0, jumpforce));
+						rb3.AddForce(new Vector2 (0, jumpforce));
+						rb4.AddForce(new Vector2 (0, jumpforce));
+					} else if (changeplayer == 2 && groundedthescript2.grounded == true) {
+						rb1.AddForce(new Vector2 (0, jumpforce));
+						rb2.AddForce(new Vector2 (0, jumpforce));
+						rb3.AddForce(new Vector2 (0, jumpforce));
+						rb4.AddForce(new Vector2 (0, jumpforce));
+					} else if (changeplayer == 3 && groundedthescript3.grounded == true) {
+						rb1.AddForce(new Vector2 (0, jumpforce));
+						rb2.AddForce(new Vector2 (0, jumpforce));
+						rb3.AddForce(new Vector2 (0, jumpforce));
+						rb4.AddForce(new Vector2 (0, jumpforce));
+					} else if (changeplayer == 4 && groundedthescript4.grounded == true) {
+						rb1.AddForce(new Vector2 (0, jumpforce));
+						rb2.AddForce(new Vector2 (0, jumpforce));
+						rb3.AddForce(new Vector2 (0, jumpforce));
+						rb4.AddForce(new Vector2 (0, jumpforce));
+					}
 				}
 				//hit.collider.attachedRigidbody.AddForce(Vector2.up);
 			}
@@ -41,21 +99,25 @@ public class ClickManager : MonoBehaviour {
 			player2.SetActive (false);
 			player3.SetActive (false);
 			player4.SetActive (false);
+			jumpforce = 500;
 		} else if (changeplayer == 2) {
 			player1.SetActive (false);
 			player2.SetActive (true);
 			player3.SetActive (false);
 			player4.SetActive (false);
+			jumpforce = 300;
 		} else if (changeplayer == 3) {
 			player1.SetActive (false);
 			player2.SetActive (false);
 			player3.SetActive (true);
 			player4.SetActive (false);
+			jumpforce = 100;
 		} else if (changeplayer == 4) {
 			player1.SetActive (false);
 			player2.SetActive (false);
 			player3.SetActive (false);
 			player4.SetActive (true);
+			jumpforce = 100;
 		} else if (changeplayer == 5) {
 			changeplayer = 1;
 		}
