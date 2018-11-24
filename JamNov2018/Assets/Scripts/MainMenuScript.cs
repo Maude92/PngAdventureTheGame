7d64d99d252019c3c82b67c660bc1a973f12c3ab
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-11-23 19:45:49 -0500

    FirstPush
    Bonne chance!

---
+++ JamNov2018/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public GameObject canvasMainUI;
+	public GameObject canvasInfosUI;
+	public GameObject canvasCreditsUI;
+
+	public void NextLevel(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void MainMenu(){
+		SceneManager.LoadScene(0);
+	}
+
+	public void QuitGame(){
+		print ("Bye bye!");
+		Application.Quit ();
+	}
+
+	public void ShowCredits(){
+		canvasMainUI.SetActive (false);
+		canvasCreditsUI.SetActive (true);
+	}
+
+	public void ReturnFromCredits(){
+		canvasCreditsUI.SetActive (false);
+		canvasMainUI.SetActive (true);
+	}
+
+	public void ShowMoreInfos(){
+		canvasMainUI.SetActive (false);
+		canvasInfosUI.SetActive (true);
+	}
+
+	public void ReturnFromInfos(){
+		canvasInfosUI.SetActive (false);
+		canvasMainUI.SetActive (true);
+	}
+}
